797cfbee654646129b880a3f25dc1a27ed8b318d
Price Hiller <philler3138@gmail.com>
2021-07-06 10:41:18 -0500

    Further encoding workaround

--- Bot/Config/config.py
+++ Bot/Config/config.py
@@ -10,9 +10,9 @@ class Config:
         self.prefix = self.config_dict["MFC-Guild"]["Prefix"]
 
     async def write(self, new_data: dict):
-        async with aiofiles.open(self.config_path, "w+", encoding="utf8") as f:
+        async with aiofiles.open(self.config_path, "w+", encoding="utf-8") as f:
             await f.write(json.dumps(new_data, indent=4, ensure_ascii=False, allow_nan=True))
 
     async def read(self) -> dict:
-        async with aiofiles.open(self.config_path, encoding="utf8") as f:
+        async with aiofiles.open(self.config_path, encoding="utf-8") as f:
             return json.loads(await f.read())
